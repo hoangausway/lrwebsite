be69ecca495c2a68dc8bb6f16780ea3a6f5a1648
hoang21 <hoang21@gmail.com>
2020-07-22 18:42:26 +1000

    redefine @media

--- src/components/sizetest.js
+++ src/components/sizetest.js
@@ -5,15 +5,17 @@ const SizeTest = () => {
   return (
     <section className={styles.sectionTest}>
       <div className={styles.testMsg}>SIZES TEST</div>
-      <p style={{ color: '#e6e7e8', padding: '2rem' }}>
-        374px mediaphonelandscape: <span style={{ color: 'pink' }}>PINK</span>
-        <br />
-        735px mediatablet: <span style={{ color: 'yellow' }}>YELLOW</span>
-        <br />
-        767px mediatabletlandscape: <span style={{ color: 'green' }}>GREEN</span>
-        <br />
-        1919px mediadesktoplarge: <span style={{ color: 'cyan' }}>CYAN</span>
-      </p>
+      <div style={{ color: '#e6e7e8', padding: '2rem' }}>
+        <p className={styles.iphone376}>$iphone376</p>
+        <p className={styles.iphonePlusP414}>$iphonePlusP414</p>
+        <p className={styles.iphonePlusL736}>$iphonePlusL736</p>
+        <p className={styles.ipadP768}>$ipadP768</p>
+        <p className={styles.ipadP834}>$ipadProP834</p>
+        <p className={styles.ipadP1024}>$ipadL1024</p>
+        <p className={styles.ipadL1112}>$ipadProL1112</p>
+        <p className={styles.ipadL1366}>$ipadProL1366</p>
+        <p className={styles.desktopL1680}>$desktopL1680</p>
+      </div>
     </section>
   )
 }
